be4958784f847bdd507e0a1c36a253800f0e1032
Evgueni Souleimanov <esoule@100500.ca>
2014-09-17 21:07:26 -0400

    powerpc: rtems/score/types.h: typedef unsigned32 as uint32_t, et al

--- cpukit/score/cpu/powerpc/rtems/score/types.h
+++ cpukit/score/cpu/powerpc/rtems/score/types.h
@@ -34,6 +34,7 @@
 #define __PPC_TYPES_h
 
 #ifndef ASM
+#include <stdint.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -43,17 +44,17 @@ extern "C" {
  *  This section defines the basic types for this processor.
  */
 
-typedef unsigned char  unsigned8;      /* unsigned 8-bit  integer */
-typedef unsigned short unsigned16;     /* unsigned 16-bit integer */
-typedef unsigned int   unsigned32;     /* unsigned 32-bit integer */
-typedef unsigned long long unsigned64; /* unsigned 64-bit integer */
+typedef uint8_t     unsigned8;      /* unsigned 8-bit  integer */
+typedef uint16_t    unsigned16;     /* unsigned 16-bit integer */
+typedef uint32_t    unsigned32;     /* unsigned 32-bit integer */
+typedef uint64_t    unsigned64; /* unsigned 64-bit integer */
 
 typedef unsigned32     Priority_Bit_map_control;
 
-typedef signed char      signed8;      /* 8-bit  signed integer */
-typedef signed short     signed16;     /* 16-bit signed integer */
-typedef signed int       signed32;     /* 32-bit signed integer */
-typedef signed long long signed64;     /* 64 bit signed integer */
+typedef int8_t      signed8;      /* 8-bit  signed integer */
+typedef int16_t     signed16;     /* 16-bit signed integer */
+typedef int32_t     signed32;     /* 32-bit signed integer */
+typedef int64_t     signed64;     /* 64 bit signed integer */
 
 typedef unsigned32 boolean;     /* Boolean value   */
 
